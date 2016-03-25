9f247b76a484451a293427ec632461f6e35ae646
azzed <azzed@DESKTOP-3JA9ICU>
2016-03-25 10:35:50 +0100

    supression test

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		Partie p=new Partie();//
 		p.initialiserPartie();
-		JFrame j=new JFrame();
-		System.out.println(j.getGraphicsConfiguration().getBounds());
+		
 	}
 
 }
