dd59d4ecd197901e0c7f2bafa9c11c8ed6327a0f
Arthur <Arthur@DESKTOP-RO08HD2>
2016-03-26 17:34:19 +0100

    Creation de la classe Personnage :)

--- src/Personnage.java
+++ src/Personnage.java
@@ -1,4 +1,18 @@
 
 public class Personnage {
-
+int energie=100;
+boolean equipe1 = false, equipe2=false;
+public Personnage(boolean equipe1){
+	if(equipe1){
+	this.equipe1 = true;
+	}else{
+		this.equipe2=true;
+	}
+}
+public String toString(){
+	if (equipe1){
+		
+	}
+	return "";
+}
 }
